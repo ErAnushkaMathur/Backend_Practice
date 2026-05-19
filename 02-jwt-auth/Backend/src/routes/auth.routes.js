ff9db4687d2ff36bdcdf9fed1f683c414113e5ee
Anushka mathur <er.anushkamathur@gmail.com>
2026-05-19 17:11:41 +0530

    feat: add password hashing using bcrypt

--- 02-jwt-auth/Backend/src/routes/auth.routes.js
+++ 02-jwt-auth/Backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
+const crypto = require("crypto")
 
 const userModel = require("../models/user.models")
 const authRouter = express.Router()
@@ -14,7 +15,9 @@ authRouter.post("/register", async (req, res) => {
       message: "With this email account user already exist"
     })
   }
-  const user = await userModel.create({ name, email, password })
+
+  const hash = crypto.createHash("md5").update(password).digest("hex")
+  const user = await userModel.create({ name, email, password : hash })
    const token = jwt.sign(
      {
       id: user._id,
@@ -40,5 +43,34 @@ authRouter.post("/protected" ,(req, res)=>{
   })
 })
 
+authRouter.post("/login" , async(req , res)=>{
+  const {email , password} = req.body
+  const user = await userModel.findOne({email})
+  
+  if(!user){
+    return res.status(404).json({
+      message: "user not found "
+    })
+  }
+  const isPasswordMatched =   user.password === crypto.createHash("md5").update(password).digest("hex")
+  if(!isPasswordMatched){
+    return res.status(401).json({
+      message: "password does not match"
+    })
+  } 
+ const token = jwt.sign({
+  id : user._id,
+  email : user._email
+ },
+ process.env.JWT_SECRET)
 
+ res.cookie("jwt" , token)
+  res.status(200).json({
+    message : "user logged in successfully",  
+    user,
+    token
+  })
+ 
+
+})
 module.exports = authRouter
