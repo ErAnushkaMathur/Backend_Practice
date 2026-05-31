07b4b0ab91e5c66172039d65861f964178b8aded
Anushka mathur <er.anushkamathur@gmail.com>
2026-05-31 23:25:01 +0530

    Implement JWT authentication for post creation and retrieval

--- 04-instagram-clone-backend/src/controllers/post.controller.js
+++ 04-instagram-clone-backend/src/controllers/post.controller.js
@@ -47,4 +47,37 @@ async function createPostController(req , res){
   
 }
 
-module.exports = {createPostController}
+async function getPostCotroller(req, res){
+
+  const token = req.cookies.token 
+
+  if(!token){
+  return res.status(401).json({
+    message: "token not provided"
+  })
+}
+  let decoded = null
+  try{
+    decoded = jwt.verify(token , process.env.JWT_SECRET)
+  }
+  catch(err){
+   return res.status(401).json({
+    message : "token is invalid"
+   })
+  }
+
+  const userId = decoded.id
+
+  const post = await postModel.find({
+    user : userId,
+  })
+
+  res.status(200).json({
+    message : "post fetched successfully",
+    post
+  })
+
+}
+module.exports = {createPostController,
+  getPostCotroller
+}
--- 04-instagram-clone-backend/src/routes/post.routes.js
+++ 04-instagram-clone-backend/src/routes/post.routes.js
@@ -5,5 +5,6 @@ const multer = require("multer")
 const upload = multer({storage : multer.memoryStorage()})
 
 postRouter.post("/",upload.single("image"),postController.createPostController)
+postRouter.get("/",postController.getPostCotroller)
 
 module.exports = postRouter
